8e20bf26fc61b06b3cf1bf48d9a721edec2e539d
Zazome <zazomazazoma@gmail.com>
2023-06-06 13:00:45 +0300

    add tiltle

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
           builder: (context,state){
             return MaterialApp(
               debugShowCheckedModeBanner: false,
-              title: '',
+              title: 'Zakazik Training',
               theme: ThemeData(
                 colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
                 useMaterial3: true,
